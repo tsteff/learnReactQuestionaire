33659c01c8eb85dac3939275f5c821c34dd6d2ed
Tim Steff <tsteff@gmail.com>
2016-05-10 00:06:59 +0200

    Only show summary box if there are answers

--- components/QuestionSummary.jsx
+++ components/QuestionSummary.jsx
@@ -12,16 +12,19 @@ constructor(props) {
       width: '150px',
       padding: '10px'
     };
+    const positiveAnswers = this.props.questionList.filter((question) => {
+        return (question.usersAnswer === 'yes')})
+    const negativeAnswers = this.props.questionList.filter((question) => {
+        return (question.usersAnswer === 'no')})
+
     return (
       <div className="row">
         <div className="col-xs-1"></div>
         <div className="col-xs-5">
           <div className="row text-center"><img src="images/yes.png" alt="smiley" style={imgStyle} /></div>
-          <div className="text-center console">
+          <div className={positiveAnswers.length > 0 ? 'text-center console' : null} >
           {
-            this.props.questionList.filter((question) => {
-                return (question.usersAnswer === 'yes');
-            }).map((question) => {
+            positiveAnswers.map((question) => {
                 return <div className="row">{question.id}. {question.questionText}</div>
             })
           }
@@ -29,11 +32,9 @@ constructor(props) {
         </div>
         <div className="col-xs-5">
           <div className="row text-center"><img src="images/no.png" alt="smiley" style={imgStyle}/></div>
-            <div className="text-center console">
+            <div className={negativeAnswers.length > 0 ? 'text-center console' : null} >
               {
-                this.props.questionList.filter((question) => {
-                    return (question.usersAnswer === 'no');
-                }).map((question) => {
+                negativeAnswers.map((question) => {
                     return <div className="row">{question.id}. {question.questionText}</div>
                 })
               }
